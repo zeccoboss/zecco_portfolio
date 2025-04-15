890a77b996e1953c09bfa283c546ec2a26b0ff73
zecco <ezekielobang@gmail.com>
2025-04-15 13:12:22 +0100

    Implement code changes to enhance functionality and improve performance

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import { useState } from 'react'
+import QuizImg from '../assets/quiz.png'
+import QuoteImg from '../assets/quote.png'
+import NoteImg from '../assets/note.png'
+import MovieImg from '../assets/movie.png'
+
 
 const Projects = () => {
     const [projects, setProjects] = useState([
@@ -8,7 +13,7 @@ const Projects = () => {
             projectName: 'Quiz App',
             description: 'Project One',
             liveLink: 'https://zeccoboss.github.io/quiz-app/',
-            imgScr: 'https://zeccoboss.github.io/quiz-app/img/icon.jpg',
+            imgScr: QuizImg,
             gitHubLink: 'https://github.com/zeccoboss/quiz-app.git'
 
         },
@@ -17,7 +22,7 @@ const Projects = () => {
             projectName: 'Fave Quote',
             description: 'Project Two',
             liveLink: 'https://zeccoboss.github.io/quote-app/',
-            imgScr: 'https://zeccoboss.github.io/quiz-app/img/icon.jpg',
+            imgScr: QuoteImg,
             gitHubLink: 'https://github.com/zeccoboss/quote-app.git'
 
         },
@@ -26,7 +31,7 @@ const Projects = () => {
             projectName: 'Note App',
             description: 'Project Three',
             liveLink: 'https://zeccoboss.github.io/note-app/',
-            imgScr: 'https://zeccoboss.github.io/quiz-app/img/icon.jpg',
+            imgScr: NoteImg,
             gitHubLink: 'https://github.com/zeccoboss/note-app.git'
 
         },
@@ -35,7 +40,7 @@ const Projects = () => {
             projectName: 'Movie Names',
             description: 'Project Four',
             liveLink: 'https://zeccoboss.github.io/movie_name/',
-            imgScr: 'https://zeccoboss.github.io/quiz-app/img/icon.jpg',
+            imgScr: MovieImg,
             gitHubLink: 'https://github.com/zeccoboss/movie_name.git'
         }
     ])
@@ -58,7 +63,7 @@ const Projects = () => {
                         <div className="project_content">
                             <h2>{project.description}</h2>
                             <div className="projects_links">
-                                <a href={project.gitHubLink} className='project_github_link'>GitHub</a>
+                                <a href={project.gitHubLink} className='project_github_link' target='_b'>GitHub</a>
                                 <a href={project.liveLink} target='_blank' className='project_live_demo'>Live Demo</a>
                             </div>
                         </div>
