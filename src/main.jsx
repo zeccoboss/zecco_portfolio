45035f8c1220da8bbc848417634b56bd65b2458c
zecco <ezekielobang@gmail.com>
2025-04-15 13:41:03 +0100

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import App from './App.jsx';
+import { HashRouter } from 'react-router-dom'; // Import HashRouter
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
+);
+
